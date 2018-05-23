3bdf70a07ca847f81a601d721e8f73a5d2560188
Francisco <fhernandez@nearsoft.com>
2018-05-23 15:23:20 -0600

    Send jira issues to standbot server

--- src/main/java/com/softwaredevtools/standbot/StandbotController.java
+++ src/main/java/com/softwaredevtools/standbot/StandbotController.java
@@ -2,20 +2,25 @@ package com.softwaredevtools.standbot;
 
 import com.atlassian.jira.bc.ServiceOutcome;
 import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.IssueManager;
+import com.atlassian.jira.issue.MutableIssue;
 import com.atlassian.jira.project.Project;
 import com.atlassian.jira.project.ProjectManager;
 import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
 import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
+import com.google.gson.reflect.TypeToken;
 import com.softwaredevtools.standbot.model.SlackIntegrationEntity;
 import com.softwaredevtools.standbot.model.mappers.SlackIntegrationMapper;
 import com.softwaredevtools.standbot.model.pojo.SlackIntegration;
 import com.google.gson.Gson;
 import com.softwaredevtools.standbot.service.SlackIntegrationService;
 import com.softwaredevtools.standbot.service.StandbotAPI;
+import org.ofbiz.core.entity.GenericEntityException;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
-import java.util.List;
+import java.lang.reflect.Type;
+import java.util.*;
 
 @Path("/")
 @Consumes({MediaType.APPLICATION_JSON})
@@ -26,12 +31,14 @@ public class StandbotController {
     private final StandbotAPI _standbotAPI;
     private Gson GSON;
     private ProjectManager _projectManager;
+    private IssueManager _issueManager;
 
     public StandbotController(SlackIntegrationService slackIntegrationService, StandbotAPI standbotAPI) {
         _slackIntegrationService = slackIntegrationService;
         _standbotAPI = standbotAPI;
         GSON = new Gson();
         _projectManager = ComponentAccessor.getProjectManager();
+        _issueManager = ComponentAccessor.getIssueManager();
     }
 
     /*
@@ -44,6 +51,128 @@ public class StandbotController {
     public Response healthcheck() {
         SlackIntegration slackIntegration = _slackIntegrationService.getSlackIntegrationForHealthCheck();
         return Response.ok(GSON.toJson(slackIntegration)).build();
+    }
+
+    @GET
+    @AnonymousAllowed
+    @Path("search")
+    public Response search(@QueryParam("projectId") Long projectId, @QueryParam("userId") String userId,
+                           @QueryParam("status") String status, @QueryParam("maxResults") String maxResults,
+                           @QueryParam("registeredJiraUrl") String registeredJiraUrl) throws GenericEntityException {
+        //option: done, in-progress, assigned
+        Collection<Long> issueIds = _issueManager.getIssueIdsForProject(projectId);
+        List<MutableIssue> issues = new ArrayList<MutableIssue>();
+
+        for (Long id : issueIds) {
+            MutableIssue issue = _issueManager.getIssueObject(id);
+
+            if (!issue.getAssigneeId().equals(userId)) {
+                continue;
+            }
+
+            if (status.equals("done")) {
+                if (!issue.getStatus().getSimpleStatus().getName().equals("Done")) {
+                    continue;
+                }
+            } else if (status.equals("in-progress")) {
+                if (!issue.getStatus().getSimpleStatus().getName().equals("In Progress")) {
+                    continue;
+                }
+            } else if (status.equals("assigned")) {
+                if (!issue.getStatus().getSimpleStatus().getName().equals("Done") && !issue.getStatus().getSimpleStatus().getName().equals("In Progress")) {
+                    continue;
+                }
+            }
+
+            issues.add(issue);
+        }
+
+        Collections.sort(issues, new Comparator<MutableIssue>() {
+            public int compare(MutableIssue o1, MutableIssue o2) {
+                return o2.getUpdated().compareTo(o2.getUpdated());
+            }
+        });
+
+        List<MutableIssue> finalList = issues.subList(0, issues.size() >= 5 ? 5 : issues.size());
+        IssueList issueList = new IssueList();
+        issueList.setIssues(finalList, registeredJiraUrl);
+
+        return Response.ok(GSON.toJson(issueList, IssueList.class)).build();
+
+    }
+
+    private class Priority {
+        private String name;
+
+        public Priority(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    private class Fields {
+        private String summary;
+        private Priority priority;
+
+        public Fields(String summary, String priorityName) {
+            this.summary = summary;
+            this.priority = new Priority(priorityName);
+        }
+
+        public Priority getPriority() {
+            return priority;
+        }
+
+        public String getSummary() {
+            return summary;
+        }
+    }
+
+    private class Issue {
+        private String key;
+        private Long id;
+        private String status;
+        private String self;
+        private Fields fields;
+
+        public Issue(MutableIssue issue, String registeredJiraUrl) {
+            key = issue.getKey();
+            id = issue.getId();
+            status = issue.getStatus().getSimpleStatus().getName();
+            self = registeredJiraUrl + "/rest/api/2/issue/" + id;
+            fields = new Fields(issue.getSummary(), issue.getPriority().getName());
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getKey() {
+            return key;
+        }
+    }
+
+    private class IssueList {
+        private List<Issue> issues;
+
+        public List<Issue> getIssues() {
+            return issues;
+        }
+
+        public void setIssues(List<MutableIssue> issues, String registeredJiraUrl) {
+            this.issues = new ArrayList<Issue>(issues.size());
+
+            for (MutableIssue originalIssue : issues) {
+                this.issues.add(new Issue(originalIssue, registeredJiraUrl));
+            }
+        }
     }
 
     @POST
